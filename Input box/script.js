5a8cd81791bfa10210adf5866f92660334d3c256
Debankush <debankushghosh@gmail.com>
2022-01-14 18:06:59 +0530

    modified

--- Input box/script.js
+++ Input box/script.js
@@ -11,7 +11,21 @@ myButton.addEventListener('click' ,(e) =>{
         myLi.innerHTML = inputText.value;
         list.appendChild(myLi);
 
-       
+        const mySpan = document.createElement('span');
+        mySpan.innerHTML = 'delete';
+        myLi.appendChild(mySpan);
+    }
+    const close = document.querySelectorAll('span');
+    for(let i=0 ; i<close.length ; i++){
+        close[i].addEventListener('click', () =>{
+            close[i].parentElement.style.opacity = 0;
+
+            setTimeout(() => {
+                
+                close[i].parentElement.style.display ='none';
+            },500);
+        })
+    
     }
    
     inputText.value = " ";
